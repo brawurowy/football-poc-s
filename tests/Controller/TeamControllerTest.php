1d2a89aaf98552d685ad45c80af5f0a00c170d2c
klazewski <klazewski@9bits.pl>
2018-07-03 15:56:43 +0200

    new tests

--- tests/Controller/TeamControllerTest.php
+++ tests/Controller/TeamControllerTest.php
@@ -4,61 +4,10 @@ namespace App\Tests\Controller;
 
 use App\Entity\Team;
 use App\Entity\League;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\ObjectRepository;
 
 class TeamControllerTest extends BaseControllerTest
 {
     public function testCreateTeam()
-    {
-        $client = $this->createAuthenticatedClient();
-        $client->request('POST', '/api/v1/teams', [
-            'name' => 'test_50',
-            'strip' => 'white',
-            'league' => 1
-        ]);
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        $data = json_decode($client->getResponse()->getContent(), true);
-
-        $this->assertArrayHasKey('success', $data);
-        $this->assertEquals(true, $data['success']);
-        $this->assertArrayHasKey('data', $data);
-    }
-
-    public function testUpdateTeam()
-    {
-        $client = $this->createAuthenticatedClient();
-
-        $client->request('PATCH', '/api/v1/teams/1', [
-            'name' => 'test_50',
-            'strip' => 'white',
-            'league' => 1
-        ]);
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        $data = json_decode($client->getResponse()->getContent(), true);
-
-        $this->assertArrayHasKey('success', $data);
-        $this->assertEquals(true, $data['success']);
-        $this->assertArrayHasKey('data', $data);
-    }
-
-    public function testDeleteTeam()
-    {
-        $client = $this->createAuthenticatedClient();
-
-        $client->request('DELETE', '/api/v1/teams/1');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-        $data = json_decode($client->getResponse()->getContent(), true);
-
-        $this->assertArrayHasKey('success', $data);
-        $this->assertEquals(true, $data['success']);
-        $this->assertArrayHasKey('data', $data);
-    }
-
-    public function testSaveTeamToDatabase()
     {
         $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
             ->disableOriginalConstructor()
@@ -77,5 +26,28 @@ class TeamControllerTest extends BaseControllerTest
 
         $this->assertEquals('Test team', $team->getName());
         $this->assertEquals('Test strip', $team->getStrip());
+    }
+
+    public function testDeleteTeam()
+    {
+        $em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $league = new League();
+        $league->setName('Liga testowa 2');
+
+        $team = new Team();
+        $team->setName('Deleted team');
+        $team->setStrip('Test strip');
+        $team->setLeague($league);
+        $league->addTeam($team);
+
+        $em->remove($team);
+        $em->remove($league);
+        $em->flush();
+
+        $this->assertEquals('Deleted team', $team->getName());
+        $this->assertEquals('Liga testowa 2', $league->getName());
     }
 }
